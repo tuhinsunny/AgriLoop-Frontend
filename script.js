a9afff103891446434cdf99eeb1a1277bf896a9b
Tuhin Chandra <tuhinchandra2k04@gmail.com>
2024-09-21 21:37:03 +0530

    Add Odometer for displaying member, waste bank, product, and education statistics

--- script.js
+++ script.js
@@ -20,3 +20,21 @@ document.querySelector('.goToSignUp').addEventListener('click', () => {
     document.querySelector('.registerform').classList.add('toggleActive');
     document.querySelector('.form-box').classList.remove('toggleLogin');
 })
+
+// Odometer 
+const createOdometer = (el, value) => {
+    const odometer = new Odometer({
+        el: el,
+        value: 0,
+    });
+    odometer.update(value);
+};
+
+const membersOdometer = document.querySelector('.members-odometer');
+createOdometer(membersOdometer, 250);
+const wasteOdometer = document.querySelector('.waste-odometer');
+createOdometer(wasteOdometer, 20);
+const productOdometer = document.querySelector('.products-odometer');
+createOdometer(productOdometer, 70);
+const educationOdometer = document.querySelector('.education-odometer');
+createOdometer(educationOdometer, 100);
